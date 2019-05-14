eb1dc98eb6b065b568d81779973e504a2e2e6f6e
Sebastian <sebastian.valencian@gmail.com>
2019-05-13 19:18:56 -0500

    [Feature]Create actionCreators.js

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,29 @@
+
+//Actions are objects
+
+//Increment we past index wich post we are going to increment
+export function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index //little info no all the information
+    }
+}
+
+//add comment
+export function addComment(postId, author, comment){
+    return {
+        type:'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+//remove comment (postId, i=whatCommentShouldIRemove)
+export function removeComment(postId, i){
+    return{
+        type:'REMOVE_COMMENT',
+        postId,
+        i
+    }
+}
