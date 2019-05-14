8a8ab134dae6a5cc24d18a856476842d6cdb0e9a
Sebastian <sebastian.valencian@gmail.com>
2019-05-13 20:27:09 -0500

    [Feature] Create the reducers

---
+++ client/reducers/posts.js
@@ -0,0 +1,14 @@
+//A reducer takes in 2 things:
+
+//1. the action(info about what happened)
+//2. Copy of current state
+//action, store
+//okay let me see
+//update and return  store
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
