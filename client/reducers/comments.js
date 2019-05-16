7f4047a8c2ed8fbdcc56ca15427fc841a2c0ab69
Sebastian <sebastian.valencian@gmail.com>
2019-05-16 18:48:38 -0500

    [Feature]Redux Reducer composition for comments reducer

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -6,10 +6,40 @@
 //okay let me see
 //update and return  store
 
-function comments(state = [], action) {
-    console.log('commentsReducers: ', state, action);
-    return state;
+function postComments(state = [], action) {
+  console.log("commentsReducers postComments: ", state, action);
+  switch (action.type) {
+    case "ADD_COMMENT":
+      // return the new state with the new comment
+      return [...state, { user: action.author, text: action.comment }];
+      break;
+    case "REMOVE_COMMENT":
+      // we need to return the new state without the deleted comment
+      return [
+        // from the start to the one we want to delete
+        ...state.slice(0, action.i),
+        // after the deleted one, to the end
+        ...state.slice(action.i + 1)
+      ];
+      break;
+
+    default:
+      return state;
   }
-  
-  export default comments;
-  
+  return state;
+}
+
+function comments(state = [], action) {
+  console.log("commentsReducers : ", state, action);
+  if (typeof action.postId !== "undefined") {
+    return {
+      //take the current state
+      ...state,
+      // overwrite this post with a new one - reducer composition
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
+  return state;
+}
+
+export default comments;
