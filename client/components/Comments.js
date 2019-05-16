6cb345d12c2dc4b387b83213c6e8a1f01eb0bdbf
Sebastian <sebastian.valencian@gmail.com>
2019-05-16 18:09:15 -0500

    [Feature] Displaying and Adding Comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 const Comments = React.createClass({
-    render(){
-        return (
-            <div className='comment'>
-                I am the comments!!
-            </div>
-        )
-    }
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  },
+
+  render() {
+    return (
+      <div className="comment">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref='commentForm' className='comment-form'>
+            <input type='text' ref='author' placeholder='author' />
+            <input type='text' ref='comment' placeholder='comment' />
+            <input type='submit' hidden />
+        </form>
+      </div>
+    );
+  }
 });
 
 export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,27 +1,27 @@
 import React from "react";
 import Photo from "./Photo";
-import Comments from './Comments';
-
+import Comments from "./Comments";
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
     // index of the post
-    const i = this.props.posts.findIndex(
-      post => post.code === this.props.params.postId
-    );
+    const i = this.props.posts.findIndex(post => post.code === postId);
     console.log("i", i);
     // get us the post
     const post = this.props.posts[i];
-    console.log('postSingle', post);
+
+    const postComments = this.props.comments[postId] || [];
+
+    console.log("postSingle", post);
 
     return (
-        // <div className="single-photo">I'm single</div>
-        <div className='single-photo'>
-            <Photo i={i}  post={post} {...this.props} />
-            <Comments />
-        </div>
-    )
-    
+      // <div className="single-photo">I'm single</div>
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
+      </div>
+    );
   }
 });
 
