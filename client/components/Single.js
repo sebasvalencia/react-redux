649502fb83e3c5bc8e71fb87104a518f2333880d
Sebastian <sebastian.valencian@gmail.com>
2019-05-16 17:43:55 -0500

    [Feature] Display Single Photo components & create comments component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,28 @@
 import React from "react";
+import Photo from "./Photo";
+import Comments from './Comments';
+
 
 const Single = React.createClass({
-    render(){
-        return(
-            <div className='single-photo'>
-                I'm single
-            </div>
-        )
-    }
+  render() {
+    // index of the post
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    console.log("i", i);
+    // get us the post
+    const post = this.props.posts[i];
+    console.log('postSingle', post);
+
+    return (
+        // <div className="single-photo">I'm single</div>
+        <div className='single-photo'>
+            <Photo i={i}  post={post} {...this.props} />
+            <Comments />
+        </div>
+    )
+    
+  }
 });
 
 export default Single;
