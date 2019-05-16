8a70a7f1e7a30d7a6251748745140a6cb0093ccb
Sebastian <sebastian.valencian@gmail.com>
2019-05-15 19:27:23 -0500

    [Feature]Display Redux state inside our components

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,18 +1,22 @@
 import React from "react";
-
+import Photo from "./Photo";
 
 const PhotoGrid = React.createClass({
-    render(){
-        return(
-            <div className='photo-grid'>
-                I'm the photogrid
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="photo-grid">
+        {/* I'm the photogrid
+        <pre>{
+            JSON.stringify(this.props.posts, null, "")
+            }
+
+        </pre> */}
+        {this.props.posts.map((post, i) => (
+          <Photo {...this.props} key={i} i={i} post={post} />
+        ))}
+      </div>
+    );
+  }
 });
 
 export default PhotoGrid;
-
-
-
-
